117a637090d7d5160856dc0f6da071501d43462f
BISHAL214 <mondalbishal716@gmail.com>
2025-09-25 00:24:41 +0530

    feat: setup logging using winston and morgan, used helmet for security

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+import logger from '#config/logger.js';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// middleware
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+// routes
 app.get('/', (req, res) => {
+  logger.info('Hello from devops practice api');
   res.status(200).send('Hello from devops practice api');
 });
 
